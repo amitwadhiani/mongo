96cf1951ceccb75b8286ae4b6727c4ce6c9ce80a
apple <akash.parmar@meddo.in>
2019-09-04 23:02:57 +0530

    Fix

--- src/main/java/co/arctern/api/provider/dao/UserTaskDao.java
+++ src/main/java/co/arctern/api/provider/dao/UserTaskDao.java
@@ -3,6 +3,7 @@ package co.arctern.api.provider.dao;
 import co.arctern.api.provider.constant.TaskState;
 import co.arctern.api.provider.domain.UserTask;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -19,7 +20,7 @@ public interface UserTaskDao extends PagingAndSortingRepository<UserTask, Long> 
      * @param state
      * @return
      */
-    Page<UserTask> findByIsActiveTrueAndUserIdAndTaskStateOrderByCreatedAtDesc(Long userId, TaskState state);
+    Page<UserTask> findByIsActiveTrueAndUserIdAndTaskStateOrderByCreatedAtDesc(Long userId, TaskState state, Pageable pageable);
 
     /**
      * find user_task for task_id.
