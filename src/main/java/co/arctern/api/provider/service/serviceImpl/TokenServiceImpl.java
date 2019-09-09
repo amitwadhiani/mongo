3851a8fa09979fc6c7f21fe940fad3fa21485201
apple <akash.parmar@meddo.in>
2019-09-09 17:36:43 +0530

    fix

--- src/main/java/co/arctern/api/provider/service/serviceImpl/TokenServiceImpl.java
+++ src/main/java/co/arctern/api/provider/service/serviceImpl/TokenServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.security.oauth2.provider.endpoint.TokenEndpoint;
