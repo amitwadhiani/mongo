5bf22de7986071e33153b74f473da9492e8ef193
apple <akash.parmar@meddo.in>
2019-09-12 12:33:47 +0530

    projection changes

--- src/main/java/co/arctern/api/provider/dto/response/projection/TasksForProvider.java
+++ src/main/java/co/arctern/api/provider/dto/response/projection/TasksForProvider.java
@@ -1,7 +1,11 @@
 package co.arctern.api.provider.dto.response.projection;
 
+import co.arctern.api.provider.constant.TaskState;
+import co.arctern.api.provider.constant.TaskType;
 import co.arctern.api.provider.domain.Task;
 import org.springframework.data.rest.core.config.Projection;
+
+import java.sql.Timestamp;
 
 /**
  * tasks of a provider response body.
@@ -10,4 +14,20 @@ import org.springframework.data.rest.core.config.Projection;
 public interface TasksForProvider {
 
     Long getId();
+
+    TaskState getState();
+
+    Timestamp getExpectedArrivalTime();
+
+    Timestamp getCreatedAt();
+
+    TaskType getType();
+
+    String getPatientName();
+
+    String getPatientPhone();
+
+    Boolean getIsPrepaid();
+
+
 }
