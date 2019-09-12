2f3c83d31db3590b4792b7c49c1d2c57019766de
apple <akash.parmar@meddo.in>
2019-09-12 12:57:46 +0530

    push

--- src/main/java/co/arctern/api/provider/controller/UserController.java
+++ src/main/java/co/arctern/api/provider/controller/UserController.java
@@ -3,14 +3,12 @@ package co.arctern.api.provider.controller;
 import co.arctern.api.provider.dto.request.UserRequestDto;
 import co.arctern.api.provider.dto.response.projection.Users;
 import co.arctern.api.provider.service.UserService;
+import org.checkerframework.checker.signature.qual.ClassGetName;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.BasePathAwareController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -36,12 +34,10 @@ public class UserController {
         return ResponseEntity.ok(userService.createUser(dto));
     }
 
-    @PostMapping("/fetch/all")
+    @GetMapping("/fetch/all")
     @CrossOrigin
     public ResponseEntity<List<Users>> fetchAll() {
         return ResponseEntity.ok(userService.fetchAll());
     }
-
-
 
 }
