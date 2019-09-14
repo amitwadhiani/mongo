918b081f7b635c1d643bc66b850ff2aac074efbe
apple <akash.parmar@meddo.in>
2019-09-14 12:09:36 +0530

    push

--- src/main/java/co/arctern/api/provider/security/AppUserDetailsService.java
+++ src/main/java/co/arctern/api/provider/security/AppUserDetailsService.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,7 +20,7 @@ import java.util.stream.Collectors;
 /**
  * Setting details in the User security model through user entity
  */
-@Component
+@Service
 public class AppUserDetailsService implements UserDetailsService {
 
     @Autowired
