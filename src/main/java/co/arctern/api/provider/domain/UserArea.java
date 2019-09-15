ce305235c71127be437f1ac5769cabc677f8425f
apple <akash.parmar@meddo.in>
2019-09-15 13:33:47 +0530

    no_t : created_At and modif. changes

--- src/main/java/co/arctern/api/provider/domain/UserArea.java
+++ src/main/java/co/arctern/api/provider/domain/UserArea.java
@@ -4,8 +4,11 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.codehaus.jackson.annotate.JsonBackReference;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
+import java.sql.Timestamp;
 
 @Entity
 @Data
@@ -24,6 +27,15 @@ public class UserArea {
     @ManyToOne
     @JsonBackReference("area-userArea")
     private Area area;
+
+    @LastModifiedDate
+    @Column(nullable = false, updatable = false, insertable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private Timestamp lastModified;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false, insertable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    private Timestamp createdAt;
+
 
     @Column(columnDefinition = "tinyint(1) DEFAULT 1", nullable = false)
     private Boolean isActive;
--- src/main/java/co/arctern/api/provider/domain/UserOffering.java
+++ src/main/java/co/arctern/api/provider/domain/UserOffering.java
@@ -4,8 +4,11 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.codehaus.jackson.annotate.JsonBackReference;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
+import java.sql.Timestamp;
 
 @Entity
 @Data
@@ -27,6 +30,15 @@ public class UserOffering {
 
     @Column(columnDefinition = "tinyint(1) DEFAULT 1", nullable = false)
     private Boolean isActive;
+
+    @LastModifiedDate
+    @Column(nullable = false, updatable = false, insertable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private Timestamp lastModified;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false, insertable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    private Timestamp createdAt;
+
 
     @Column(nullable = false, columnDefinition = "bigint(20) DEFAULT 1")
     @Version
