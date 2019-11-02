5304b2f1badb1c44b759c6df8867fcc984f5ff68
apple <akash.parmar@meddo.in>
2019-11-02 15:44:08 +0530

    Fix

--- src/main/java/co/arctern/api/provider/service/serviceImpl/GenericServiceImpl.java
+++ src/main/java/co/arctern/api/provider/service/serviceImpl/GenericServiceImpl.java
@@ -1,22 +1,40 @@
 package co.arctern.api.provider.service.serviceImpl;
 
+import co.arctern.api.provider.constant.SettleState;
+import co.arctern.api.provider.constant.TaskState;
+import co.arctern.api.provider.dao.TaskDao;
+import co.arctern.api.provider.domain.Task;
+import co.arctern.api.provider.dto.response.projection.Payments;
 import co.arctern.api.provider.service.GenericService;
-import co.arctern.api.provider.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.projection.ProjectionFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class GenericServiceImpl implements GenericService {
 
-    private final TaskService taskService;
+    private final TaskDao taskDao;
+    private final ProjectionFactory projectionFactory;
 
     @Autowired
-    public GenericServiceImpl(TaskService taskService) {
-        this.taskService = taskService;
+    public GenericServiceImpl(TaskDao taskDao,
+                              ProjectionFactory projectionFactory) {
+        this.taskDao = taskDao;
+        this.projectionFactory = projectionFactory;
     }
 
     @Override
     public Double fetchUserOwedAmount(Long userId) {
-        return taskService.fetchUserOwedAmount(userId);
+        List<Task> tasks = taskDao.findByActiveUserIdAndState(userId, TaskState.COMPLETED);
+        List<Payments> payments = new ArrayList<>();
+        tasks.stream().forEach(a -> {
+            payments.addAll(a.getPayments().stream().filter(b -> b.getSettleState().equals(SettleState.REQUESTED))
+                    .map(c -> projectionFactory.createProjection(Payments.class, c)).collect(Collectors.toList()));
+        });
+        return payments.stream().mapToDouble(a -> a.getAmount()).sum();
     }
 }
