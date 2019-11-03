4ca2a4daac90d5924a9d0ceac0605c2a5c0ef070
apple <akash.parmar@meddo.in>
2019-11-03 15:58:06 +0530

    New entity services

---
+++ src/main/java/co/arctern/api/provider/service/serviceImpl/PaymentStateFlowServiceImpl.java
@@ -0,0 +1,8 @@
+package co.arctern.api.provider.service.serviceImpl;
+
+import co.arctern.api.provider.service.PaymentStateFlowService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentStateFlowServiceImpl implements PaymentStateFlowService {
+}
---
+++ src/main/java/co/arctern/api/provider/service/serviceImpl/SettleStateFlowServiceImpl.java
@@ -0,0 +1,8 @@
+package co.arctern.api.provider.service.serviceImpl;
+
+import co.arctern.api.provider.service.SettleStateFlowService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SettleStateFlowServiceImpl implements SettleStateFlowService {
+}
