bd2b128199665b2cb692dd7b881eeaa8780f3bac
apple <akash.parmar@meddo.in>
2020-01-07 17:04:01 +0530

    Phase4 : fix

--- src/main/java/co/arctern/api/provider/dto/response/projection/Users.java
+++ src/main/java/co/arctern/api/provider/dto/response/projection/Users.java
@@ -2,8 +2,11 @@ package co.arctern.api.provider.dto.response.projection;
 
 import co.arctern.api.provider.constant.Gender;
 import co.arctern.api.provider.domain.User;
+import co.arctern.api.provider.domain.UserCluster;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.rest.core.config.Projection;
+
+import java.util.List;
 
 /**
  * user entity default response body.
@@ -31,5 +34,7 @@ public interface Users {
     Gender getGender();
 
     Integer getAge();
+
+    List<UserCluster> getUserClusters();
 
 }
