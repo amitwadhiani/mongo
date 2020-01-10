692c03b5f596612b05266575435a09e05b7159b9
apple <akash.parmar@meddo.in>
2020-01-10 11:38:11 +0530

    Patient name added

--- src/main/java/co/arctern/api/provider/dto/response/projection/PaymentsForUser.java
+++ src/main/java/co/arctern/api/provider/dto/response/projection/PaymentsForUser.java
@@ -3,8 +3,10 @@ package co.arctern.api.provider.dto.response.projection;
 import co.arctern.api.provider.constant.PaymentState;
 import co.arctern.api.provider.constant.SettleState;
 import co.arctern.api.provider.domain.Payment;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.rest.core.config.Projection;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 /**
@@ -16,6 +18,11 @@ public interface PaymentsForUser {
     Long getId();
 
     Double getAmount();
+
+    Timestamp getCreatedAt();
+
+    @Value("#{target.task.patientName}")
+    String getPatientName();
 
     SettleState getSettleState();
 
